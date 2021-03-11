9a9e2d43e7ad9d25e786a16b0d54dd3ed0fe2d76
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2021-03-11 20:46:21 -0300

    Initial commit.

---
+++ SharePointBackup.ps1
@@ -0,0 +1,26 @@
+﻿# User defined variables
+$tenant = "digitalfortytwo";
+$sourceSite = "/sites/PnPFonte";
+$templateName = "Template de Testes";
+
+# Constants
+$webUrl = "https://{0}.sharepoint.com{1}" -f $tenant, $sourceSite;
+$templatePath = ".\Result\Backup.xml";
+
+# Office 365 Login 
+Write-Output $("Connecting to {0}..." -f $webUrl);
+Connect-PnPOnline -Url $webUrl -Interactive;
+     
+# Save Template
+Write-Output $("Saving PnP template to {0}..." -f $templatePath);
+Get-PnPSiteTemplate `
+    -Out $templatePath `
+    -IncludeAllTermGroups `
+    -IncludeSiteCollectionTermGroup `
+    -IncludeSiteGroups `
+    -PersistBrandingFiles `
+    -PersistPublishingFiles `
+    -IncludeNativePublishingFiles `
+    -IncludeAllPages `
+    -TemplateDisplayName $templateName `
+    -Force;
